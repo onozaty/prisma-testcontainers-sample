290d9f4111e1fb788762048eed663e05f5dfe464
onozaty <onozaty@gmail.com>
2025-04-19 17:11:02 +0900

    Prisma、Vitest設定

---
+++ prisma/migrations/20250419070926_update_comments/migration.sql
@@ -0,0 +1,22 @@
+-- Prisma Database Comments Generator v1.1.0
+
+-- customers comments
+COMMENT ON TABLE "customers" IS 'Customer';
+COMMENT ON COLUMN "customers"."customer_id" IS 'Customer ID';
+COMMENT ON COLUMN "customers"."name" IS 'Customer Name';
+COMMENT ON COLUMN "customers"."email" IS 'e-mail';
+
+-- products comments
+COMMENT ON TABLE "products" IS 'Product';
+COMMENT ON COLUMN "products"."product_id" IS 'Product ID';
+COMMENT ON COLUMN "products"."name" IS 'Product Name';
+COMMENT ON COLUMN "products"."description" IS 'Product Description';
+COMMENT ON COLUMN "products"."price" IS 'Price';
+
+-- sales comments
+COMMENT ON TABLE "sales" IS 'Sale';
+COMMENT ON COLUMN "sales"."sale_id" IS 'Sale ID';
+COMMENT ON COLUMN "sales"."customer_id" IS 'Customer ID';
+COMMENT ON COLUMN "sales"."product_id" IS 'Product ID';
+COMMENT ON COLUMN "sales"."quantity" IS 'Quantity';
+COMMENT ON COLUMN "sales"."total_price" IS 'Total Price';
