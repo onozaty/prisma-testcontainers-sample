9c210c58f1d929f4bc77b80919da421df58cfe96
onozaty <onozaty@gmail.com>
2025-04-20 19:45:33 +0900

    ファイル監視によるテスト実行を無効(watch: false)に

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    globalSetup: "./test/global-setup.ts",
+    globalSetup: ["./test/global-setup.ts"],
     setupFiles: ["./test/setup.ts"],
+    watch: false,
     hookTimeout: 600000, // Testcontainerの起動に初回は時間がかかる可能性があるため
     minWorkers: 4,
     maxWorkers: 4,
